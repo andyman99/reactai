f31ae0f3999ee603e858dab96381344b70893c2c
andyman99 <andreas.heier.1999@gmail.com>
2025-03-03 14:09:00 +0100

    Fixed flaw with sidebar folder
    Foldername was sidebar instead of Sidebar, leading to import fail

--- reactai/src/components/sidebar/Sidebar.js
+++ reactai/src/components/sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as contentful from 'contentful';
-import { Card } from '../../components'; // Import the Card component
+import { Card } from '..'; // Import the Card component
 import './Sidebar.css';
 import { Link } from 'react-router-dom';
 
