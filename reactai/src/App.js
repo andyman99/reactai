bba94d104a288d82f3af7a220c263261edaee8a5
andyman99 <andreas.heier.1999@gmail.com>
2025-03-04 12:32:59 +0100

    Changing routing to check reloading pages

--- reactai/src/App.js
+++ reactai/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home, About, Events, Contact, Blog } from "./views";
-import { Layout } from './components';  // Layout component wrapping everything
 import './App.css';  // Link to App.css
 import 'leaflet/dist/leaflet.css';  // Import Leaflet CSS globally
+import { Header, Footer, Sidebar } from "./components";  // Import Header, Footer, and Sidebar components
+
 
 
 function App() {
   return (
     <Router>
-      <Layout>
+      <Header />
+      <main>
         <Routes>
           {/* Static routes */}
           <Route path="/" element={<Home />} />
@@ -20,7 +22,9 @@ function App() {
           {/* Blog listing and dynamic blog post details */}
           <Route path="/blog/*" element={<Blog />} />  {/* Keep dynamic routing for blog */}
         </Routes>
-      </Layout>
+      </main>
+      <Sidebar />
+      <Footer />
     </Router>
   );
 }
