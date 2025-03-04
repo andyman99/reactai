a8185da09d251a34e949d668f6a6630ea4e6d127
andyman99 <andreas.heier.1999@gmail.com>
2025-03-04 13:02:38 +0100

    Redirect actually added

--- reactai/src/App.js
+++ reactai/src/App.js
@@ -18,8 +18,6 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/events/*" element={<Events />} />  {/* Add the "/*" here to handle child routes */}
           <Route path="/contact" element={<Contact />} />
-          
-          {/* Blog listing and dynamic blog post details */}
           <Route path="/blog/*" element={<Blog />} />  {/* Keep dynamic routing for blog */}
         </Routes>
       </main>
